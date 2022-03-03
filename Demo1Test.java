86da9898aad37460d23e572adeb40e675ba21d37
Rami Maalouf <Ramim66809@gmail.com>
2022-03-03 22:18:05 +0400

    Update Demo1Test.java

--- Demo1Test.java
+++ Demo1Test.java
@@ -2,54 +2,59 @@ package project;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Demo1Test {
 
-
     @Test
     void capitalize() {
+        String str = "canada";
+        assertEquals("Canada", Demo1.capitalize(str));
     }
 
     @Test
     void add_player() {
+        Demo1.add_player("Rami", "Maalouf", "SG", "Warriors");
+        assertEquals("{Maalouf={games played=0, rebounds=0, made ft=0, missed 2pt=0, missed 3pt=0, blocks=0, team=Warriors, steals=0, Name=Rami, fam name=Maalouf, made 3pt=0, made 2pt=0, assists=0, missed ft=0, position=SG}}",Demo1.players.toString());
     }
 
     @Test
     void add_data() {
+        Demo1.add_player("Rami", "Maalouf", "SG", "Warriors");
+
+        Demo1.add_data("Maalouf", 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+        assertEquals("{Maalouf={games played=5, rebounds=5, made ft=5, missed 2pt=5, missed 3pt=5, blocks=5, team=Warriors, steals=5, Name=Rami, fam name=Maalouf, made 3pt=5, made 2pt=5, assists=5, missed ft=5, position=SG}}",Demo1.players.toString());
     }
 
     @Test
-    void above_certain() {
+    void above_certain1() {
+        Demo1.add_player("Rami", "Maalouf", "SG", "Warriors");
+        Demo1.add_data("Maalouf", 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+
+        // checks if fam name will be added to top_players arrayList when cutoff is lower than player's data
+        Demo1.above_certain(5,4,4);
+        assertEquals("[Maalouf]",Demo1.top_players.toString());
     }
 
     @Test
-    void get_best_worst() {
+    void above_certain2() {
+        Demo1.add_player("Rami", "Maalouf", "SG", "Warriors");
+        Demo1.add_data("Maalouf", 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+
+        // Checks if the cutoff is also included
+        Demo1.above_certain(6,5,5);
+        assertEquals("[Maalouf]",Demo1.top_players.toString());
     }
 
-
-
     @Test
-    void best_team() {
-        List<String> best_team = project.Demo1.best_team();
-        List<String> best_team_test = new ArrayList<String>();
+    void above_certain3() {
+        Demo1.add_player("Rami", "Maalouf", "SG", "Warriors");
+        Demo1.add_data("Maalouf", 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
 
-                //best_team_test is expected, best_team actual
-                assertEquals(best_team_test, best_team);
-
-    }
-
-
-    @Test
-    void best_defender() {
-        List<String> best_defender = project.Demo1.best_defender();
-        List<String> best_defender_test = new ArrayList<String>();
-
-        //best_team_test is expected, best_team actual
-        assertEquals(best_defender_test, best_defender);
-
+         // checks if fam name will be added to top_players arrayList when cutoff is higher than player's data
+        Demo1.above_certain(7,5,5);
+        assertEquals("[]",Demo1.top_players.toString());
     }
 }
