920d092f165dfaf8993fcfb668d89cd535fab9cc
maxk4 <99052906+maxk4@users.noreply.github.com>
2022-03-03 10:46:17 -0700

    Update Demo1Test.java

--- Demo1Test.java
+++ Demo1Test.java
@@ -2,9 +2,13 @@ package project;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Demo1Test {
+
 
     @Test
     void capitalize() {
@@ -26,11 +30,26 @@ class Demo1Test {
     void get_best_worst() {
     }
 
+
+
     @Test
     void best_team() {
+        List<String> best_team = project.Demo1.best_team();
+        List<String> best_team_test = new ArrayList<String>();
+
+                //best_team_test is expected, best_team actual
+                assertEquals(best_team_test, best_team);
+
     }
+
 
     @Test
     void best_defender() {
+        List<String> best_defender = project.Demo1.best_defender();
+        List<String> best_defender_test = new ArrayList<String>();
+
+        //best_team_test is expected, best_team actual
+        assertEquals(best_defender_test, best_defender);
+
     }
 }
