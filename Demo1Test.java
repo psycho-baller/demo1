d2469a1433cbfe979c0e3ad1036f876e01e29e32
Rami Maalouf <Ramim66809@gmail.com>
2022-03-02 21:48:22 -0700

    Add files via upload

---
+++ Demo1Test.java
@@ -0,0 +1,36 @@
+package project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Demo1Test {
+
+    @Test
+    void capitalize() {
+    }
+
+    @Test
+    void add_player() {
+    }
+
+    @Test
+    void add_data() {
+    }
+
+    @Test
+    void above_certain() {
+    }
+
+    @Test
+    void get_best_worst() {
+    }
+
+    @Test
+    void best_team() {
+    }
+
+    @Test
+    void best_defender() {
+    }
+}
